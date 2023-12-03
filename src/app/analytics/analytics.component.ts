56d87df08960bc5db97fb2f906a6c7fb4233ca34
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2023-12-03 19:12:24 +0100

    Parsing the csv file

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { FileUploadModule } from 'primeng/fileupload';
 import { ButtonModule } from 'primeng/button';
 import { HttpEvent } from '@angular/common/http';
+import Papa from 'papaparse';
 
 interface UploadEvent {
   originalEvent: HttpEvent<any>;
   files: File[];
+}
+
+interface SaleItem {
+  day: string;
+  product: string;
+  basePrice: number;
+  netUnits: number;
 }
 
 @Component({
@@ -18,8 +26,30 @@ interface UploadEvent {
 
 export class AnalyticsComponent {
   uploadedFile?: File;
+  items: SaleItem[] = [];
 
   onFileUpload(event: UploadEvent) {
     this.uploadedFile = event.files[0];
+  }
+
+  createModel() {
+    if(!this.uploadedFile) {
+      return;
+    }
+
+    Papa.parse(this.uploadedFile, {
+      header: true,
+      skipEmptyLines: true,
+      complete: (results) => {
+        const data = results.data as any[];
+        this.items = data.map(row => ({
+          day: row['Day'],
+          product: row['Product'],
+          basePrice: row['Base Price'],
+          netUnits: row['Net Units']
+        }));
+        this.items.pop();
+      }
+    });
   }
 }
