02bdfbfdd22d472c829761f605d1c2c37ea741d1
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2023-12-03 15:23:00 +0100

    Uploading the sales report csv file

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ButtonModule } from 'primeng/button';
+import { HttpEvent } from '@angular/common/http';
+
+interface UploadEvent {
+  originalEvent: HttpEvent<any>;
+  files: File[];
+}
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
-  imports: [],
+  imports: [ FileUploadModule, ButtonModule ],
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.scss'
 })
-export class AnalyticsComponent {
 
+export class AnalyticsComponent {
+  uploadedFile?: File;
+
+  onFileUpload(event: UploadEvent) {
+    this.uploadedFile = event.files[0];
+  }
 }
