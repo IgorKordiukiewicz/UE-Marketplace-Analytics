ec37c6fcb70719568957f736efec22528e60b1b0
Igor Kordiukiewicz <igorkordiukiewicz@gmail.com>
2023-12-03 14:48:03 +0100

    Created Analytics component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./analytics/analytics.component').then(mod => mod.AnalyticsComponent) }
+];
